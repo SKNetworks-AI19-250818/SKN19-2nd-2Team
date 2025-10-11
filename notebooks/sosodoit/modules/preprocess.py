04b576224281256abf221bd3615dd69943756a3d
sosodoit <rosesh0218@gmail.com>
2025-10-11 16:00:16 +0900

    chore: ml 파일명 수정, json 경로 수정

--- notebooks/sosodoit/modules/preprocess.py
+++ notebooks/sosodoit/modules/preprocess.py
@@ -1,15 +1,13 @@
-import warnings
-warnings.filterwarnings('ignore')
-
+# ==================================================
+# 내용: eda | ml 에서 사용된 전처리 코드
+# 데이터 카테고리: 기본정보 + 가구정보
+# ==================================================
 import pandas as pd
 import numpy as np
 
-from modules.columns import cols, code_maps
-usecols = list(cols.keys())
-
 # 데이터 로드
 filepath = "../../data/analy_data.csv"
-data = pd.read_csv(filepath, usecols=usecols, encoding="utf-8")
+data = pd.read_csv(filepath, encoding="utf-8")
 
 # 데이터 전처리
 data['EXAMIN_YEAR'] = data['EXAMIN_YEAR'].apply(lambda x: x.split("'")[1])
@@ -37,7 +35,7 @@ data['fma_14z1'] = data['fma_14z1'].replace({77777: np.nan, 88888:np.nan, 99999:
 data['fma_24z2'] = data['fma_24z2'].replace({'77': np.nan, '88':np.nan, '99': np.nan})
 data['nue_01z1'] = data['nue_01z1'].replace({7: np.nan, 9: np.nan})
 
-# 파생변수 생성
+# 파생변수
 def outliers_iqr(df, col):
     q1 = df[col].quantile(0.25)
     q3 = df[col].quantile(0.75)
@@ -45,7 +43,6 @@ def outliers_iqr(df, col):
     upper =  np.floor(q3 + 1.5 * iqr)
     new_col = f'{col}_iqr'
     df[new_col] = np.where(df[col] > upper, upper, df[col])
-    print(f'{new_col} >>> Q1: {q1}, Q3: {q3}, IQR: {iqr}, Upper Bound: {upper}')
     return df
 
 # 가구원수그룹
@@ -54,8 +51,11 @@ data = outliers_iqr(data, 'mbhld_co')
 # 연령대
 data['age_group'] = (data['age'] // 10) * 10
 
-# 성인 1인가구
+# 성인 1인가구(x)
 data['is_single_adult'] = np.where((data['mbhld_co'] == 1) & (data['reside_adult_co'] == 1), 1, 0)
+
+# 1인가구
+data['is_single'] = np.where((data['mbhld_co'] == 1), 1, 0)
 
 # 월간소득 기준 가구소득컬럼 통합
 data['fma_14z1_add'] = data['fma_14z1'] 
@@ -67,10 +67,10 @@ labels = [1, 2, 3, 4, 5, 6, 7, 8]
 data['fma_14z1_group'] = pd.cut(data['fma_14z1_add'], bins=bins, labels=labels, right=False)
 data['fma_14z1_group'] = np.where(data['fma_14z1_group'].isna(), data['fma_24z2'].astype(float), data['fma_14z1_group'])
 
-# 소득 5분위
+# 소득 5분위(x)
 data['fma_14z1_5q'] = pd.qcut(data['fma_14z1_add'], 5, labels=['1Q','2Q','3Q','4Q','5Q'])
 
-# 식생활균형
+# 식생활균형(x)
 data['food_stable'] = np.where(data['nue_01z1'].isin(['1', '2']), 1, np.where(data['nue_01z1'].isin(['3', '4']), 0, np.nan))
 
 # 치매가족여부
@@ -85,3 +85,9 @@ def get_dementia_case(row):
         return np.nan
 
 data['fma_dementia_case'] = data.apply(get_dementia_case, axis=1)
+
+# 일반담배일평균(단위: 개비)
+data['smb_avg_per_day'] = data.apply(lambda x : x[['smb_01z1', 'smb_03z1', 'smb_06z1']].max(skipna=True), axis=1)
+
+# 컬럼 삭제
+data.drop(['fma_13z1', 'fma_14z1','fma_27z1','fma_26z1','smb_01z1','smb_03z1','smb_06z1'], axis=1, inplace=True)
